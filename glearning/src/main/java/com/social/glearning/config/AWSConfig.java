ae9bbcf34652ce1132b2f43594fcb237ddc068f1
Rakesh100908 <rakeshk5@cisco.com>
2019-10-02 14:38:04 +0530

    rakesh commit for s3

--- glearning/src/main/java/com/social/glearning/config/AWSConfig.java
+++ glearning/src/main/java/com/social/glearning/config/AWSConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
@@ -15,7 +16,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
 @Configuration
-public class DynamoDbConfig {
+public class AWSConfig {
 
 	@Value("${amazon.end-point.url}")
 	private String dBEndpoint;
@@ -25,6 +26,12 @@ public class DynamoDbConfig {
 
 	@Value("${amazon.access.secretkey}")
 	private String secretKey;
+	
+	@Value("${aws.region}")
+    private String awsRegion;
+
+    @Value("${aws.s3.audio.bucket}")
+    private String awsS3AudioBucket;
 
 	
 	@Bean
@@ -39,10 +46,27 @@ public class DynamoDbConfig {
 				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(dBEndpoint,Region.getRegion(Regions.US_WEST_2).getName()))
 				.build();
 	}
+	
+	@Bean(name = "awsCredentialsProvider")
+    public AWSCredentialsProvider getAWSCredentials() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        return new AWSStaticCredentialsProvider(awsCredentials);
+    }
 
 	@Bean
 	public AWSCredentials amazonAWSCredentials() {
 		return new BasicAWSCredentials(accessKey, secretKey);
 	}
-
+	
+	@Bean(name = "awsRegion")
+    public Region getAWSPollyRegion() {
+        return Region.getRegion(Regions.fromName(awsRegion));
+    }
+	
+	@Bean(name = "awsS3AudioBucket")
+    public String getAWSS3AudioBucket() {
+        return awsS3AudioBucket;
+    }
 }
+
+
